ec148f19e1116c8e43dd177b6be58db674b2da6d
Max Sidorenko <fivemoshr@gmail.com>
2023-05-20 21:35:15 +0300

    fix(core):change base url

--- src/redux/store/axiosBaseUrl.ts
+++ src/redux/store/axiosBaseUrl.ts
@@ -1,32 +1,35 @@
-import { BaseQueryFn } from "@reduxjs/toolkit/query";
-import axios, { AxiosRequestConfig, AxiosError } from "axios";
+import { BaseQueryFn } from '@reduxjs/toolkit/query'
+import axios, { AxiosRequestConfig, AxiosError } from 'axios'
+
+export const BASE_URL =
+  import.meta.env.VITE_API_URL || 'https://646876a360c8cb9a2caa54ef.mockapi.io/api'
 
 export const axiosBaseQuery =
   (
-    { baseUrl }: { baseUrl: string } = { baseUrl: "" }
+    { baseUrl }: { baseUrl: string } = { baseUrl: '' },
   ): BaseQueryFn<
     {
-      url: string;
-      method: AxiosRequestConfig["method"];
-      data?: AxiosRequestConfig["data"];
-      params?: AxiosRequestConfig["params"];
+      url: string
+      method: AxiosRequestConfig['method']
+      data?: AxiosRequestConfig['data']
+      params?: AxiosRequestConfig['params']
     },
     unknown,
     unknown
   > =>
   async ({ url, method, data, params }) => {
     try {
-      const result = await axios({ url: baseUrl + url, method, data, params });
+      const result = await axios({ url: baseUrl + url, method, data, params })
 
-      return { data: result.data };
+      return { data: result.data }
     } catch (axiosError) {
-      const err = axiosError as AxiosError;
+      const err = axiosError as AxiosError
 
       return {
         error: {
           status: err.response?.status,
           data: err.response?.data || err.message,
         },
-      };
+      }
     }
-  };
+  }
--- src/redux/users/usersApi.ts
+++ src/redux/users/usersApi.ts
@@ -1,47 +1,47 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 
-import {axiosBaseQuery} from "redux/store/axiosBaseUrl";
+import {BASE_URL, axiosBaseQuery} from "redux/store/axiosBaseUrl";
 import { IUser } from "types/users";
 
 export const usersApi = createApi({
-  reducerPath: "users",
+  reducerPath: 'users',
   baseQuery: axiosBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL,
+    baseUrl: BASE_URL,
   }),
-  tagTypes: ["users"],
+  tagTypes: ['users'],
   endpoints: (builder) => ({
     getUsers: builder.query<IUser[], void>({
       query: () => ({
-        url: "/users",
-        method: "get",
+        url: '/users',
+        method: 'get',
       }),
-      providesTags: ["users"],
+      providesTags: ['users'],
     }),
-    toggleFollow: builder.mutation<IUser, Pick<IUser, "id"> & Partial<IUser>>({
+    toggleFollow: builder.mutation<IUser, Pick<IUser, 'id'> & Partial<IUser>>({
       query: ({ id, ...patch }) => ({
         url: `/users/${id}`,
-        method: "PUT",
+        method: 'PUT',
         data: patch,
       }),
       async onQueryStarted({ id }, { queryFulfilled, dispatch }) {
         try {
-          const { data: updatedProduct } = await queryFulfilled;
+          const { data: updatedProduct } = await queryFulfilled
           dispatch(
-            usersApi.util.updateQueryData("getUsers", undefined, (draft) => {
-              const foundedObject = draft?.find((item: IUser) => item.id === id);
+            usersApi.util.updateQueryData('getUsers', undefined, (draft) => {
+              const foundedObject = draft?.find((item: IUser) => item.id === id)
 
               if (foundedObject !== undefined) {
-                foundedObject.follow = updatedProduct.follow;
-                foundedObject.followers = updatedProduct.followers;
+                foundedObject.follow = updatedProduct.follow
+                foundedObject.followers = updatedProduct.followers
               }
-            })
-          );
+            }),
+          )
         } catch (error) {
-          console.log("catch error:", error);
+          console.log('catch error:', error)
         }
       },
     }),
   }),
-});
+})
 
 export const { useGetUsersQuery, useToggleFollowMutation } = usersApi;
