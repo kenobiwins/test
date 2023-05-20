1f3780613b5fcd09c40dfa349d7870e2c77c1949
Max Sidorenko <fivemoshr@gmail.com>
2023-05-20 22:20:42 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      components: "/src/components",
-      pages: "/src/pages",
-      store: "/src/redux",
-      theme: "/src/theme",
-      hooks: "/src/hooks",
-      types: "/src/types",
-      utils: "/src/utils",
-      kit:"/src/kit"
+      components: '/src/components',
+      pages: '/src/pages',
+      store: '/src/redux',
+      theme: '/src/theme',
+      hooks: '/src/hooks',
+      types: '/src/types',
+      utils: '/src/utils',
+      kit: '/src/kit',
     },
   },
   build: {
-    outDir:'./dist'
-  }
-});
+    assetsDir: './dist',
+  },
+})
