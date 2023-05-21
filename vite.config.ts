8029d180aa7a12399a9a2a41eb2dae70ae8ee9c3
Max Sidorenko <fivemoshr@gmail.com>
2023-05-21 14:06:51 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   
   plugins: [react(), tsconfigPaths()],
-  base: '/tweets/',
+  base: '/tweets',
   resolve: {
     alias: {
       components: '/src/components',
