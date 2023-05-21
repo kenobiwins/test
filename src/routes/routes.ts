aa5e360ae61f1dedd0b54cd71a2573f60cd99e27
Max Sidorenko <fivemoshr@gmail.com>
2023-05-21 13:53:11 +0300

    fix(core):routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,4 @@
 export enum ROUTES {
-  MAIN = "/home",
+  MAIN = "/",
   TWEETS = "/tweets",
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react(), tsconfigPaths()],
-  base: '/',
+  base: '/tweets',
   resolve: {
     alias: {
       components: '/src/components',
