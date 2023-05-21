d0ca34c40596c86e2bf9dc71abcb4136d3486ea4
Max Sidorenko <fivemoshr@gmail.com>
2023-05-21 13:29:01 +0300

    test

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,15 @@
-import { NavLink } from "react-router-dom";
-import { styled } from "styled-components";
+import { NavLink } from 'react-router-dom'
+import { styled } from 'styled-components'
 
-import { ROUTES } from "routes";
+import { ROUTES } from 'routes'
 
 const NavItems = [
   {
-    name: "Home",
+    name: 'Home',
     path: ROUTES.MAIN,
   },
-  { name: "Tweets", path: ROUTES.TWEETS },
-];
+  { name: 'Tweets', path: ROUTES.TWEETS },
+]
 
 export const Navigation = () => {
   return (
@@ -19,39 +19,39 @@ export const Navigation = () => {
           <NavItem key={name} to={path}>
             {name}
           </NavItem>
-        );
+        )
       })}
     </Nav>
-  );
-};
+  )
+}
 
 export const Header = styled.header(({ theme }) => ({
   borderBottom: `1px solid ${theme.color.accent}`,
   boxShadow: `0px 7px 12px 0px ${theme.color.accent}`,
-}));
+}))
 
 export const Nav = styled.nav(({ theme }) => ({
-  display: "flex",
-  gap: theme.px.x3 + "px",
-}));
+  display: 'flex',
+  gap: theme.px.x3 + 'px',
+}))
 
 export const NavItem = styled(NavLink)(({ theme }) => ({
-  textDecoration: "none",
+  textDecoration: 'none',
   color: theme.color.mainText,
-  padding: theme.px.x3 + "px",
-  fontSize: "1rem",
+  padding: theme.px.x3 + 'px',
+  fontSize: '1rem',
   border: `1px solid ${theme.color.mainText}`,
   borderRadius: theme.px.x3,
-  transition: "color 200ms linear , border-color 200ms linear",
+  transition: 'color 200ms linear , border-color 200ms linear',
 
-  "&.active": {
+  '&.active': {
     color: theme.color.accent,
-    textDecoration: "underline",
+    textDecoration: 'underline',
     borderColor: theme.color.accent,
   },
 
-  "&:not(.active):hover, &:not(.active):focus": {
+  '&:not(.active):hover, &:not(.active):focus': {
     color: theme.color.accent,
     borderColor: theme.color.accent,
   },
-}));
+}))
