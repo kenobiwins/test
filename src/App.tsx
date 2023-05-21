d0f44da9a1f13309e14518da20993ca4cc30c10e
Max Sidorenko <fivemoshr@gmail.com>
2023-05-21 13:57:26 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
+      <Route path="" element={<Layout />}>
+        <Route index path={ROUTES.MAIN} element={<HomePage />} />
         <Route path={ROUTES.TWEETS} element={<TweetsPage />} />
         <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
